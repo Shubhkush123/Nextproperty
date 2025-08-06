12ad79fa02b6d59af2d7509d7d78ce76854b0da1
shubham kushwah <Shubhamkushwah3753@gmail.com>
2025-08-06 12:47:27 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:3000'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:3000' ,'https://nextproperty-vv3f.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
